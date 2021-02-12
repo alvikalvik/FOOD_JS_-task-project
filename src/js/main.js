23bf97534b853273a783250ab9a763d45af4380d
alvikalvik <alvikalvik@gmail.com>
2021-02-12 14:14:07 +0200

    tabs done (task 38)

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsParent = document.querySelector('.tabheader__items');
+    const tabs = tabsParent.querySelectorAll('.tabheader__item');
+    const tabContents = document.querySelectorAll('.tabcontent');
+
+    function hideTabs() {
+        for (const item of tabs) {
+            item.classList.remove('tabheader__item_active');
+        }
+
+        for (const item of tabContents) {
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+        }
+    }
+
+    function showTab(i = 0) {
+        tabs[i].classList.add('tabheader__item_active');
+        tabContents[i].classList.add('show', 'fade');
+        tabContents[i].classList.remove('hide');
+    }
+
+    hideTabs();
+    showTab();
+
+    tabsParent.addEventListener('click', evt => {
+        const target = evt.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target === item) {
+                    hideTabs();
+                    showTab(i);
+                }
+            });
+        }
+
+        
+    });
+
+
+
+
+});
+
